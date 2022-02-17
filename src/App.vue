9e3892c9d6ea867d11167d0b734639b0396f3593
gabriarcu <38346968+gabriarcu@users.noreply.github.com>
2022-02-17 20:29:52 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+
+body{
+  background-color : #27292d;
+}
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h4{
+  text-align: center;
+    opacity: .5;
+    margin: 0;
+}
+
+.done{
+  text-decoration: line-through;
+}
+
+.form-switch .form-check-input:checked {
+    background-color: #a0a4d9;
+    border-color: #a0a4d9;
+    border: none;
+}
+
+</style>
